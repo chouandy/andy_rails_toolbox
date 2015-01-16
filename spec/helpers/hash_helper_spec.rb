1a8ec147edc6b166fca7a91078a3858acebf3ca0
ChouAndy <chouandy625@gmail.com>
2015-01-16 20:31:18 +0800

    Rewrite Helper Rspec

--- spec/helpers/hash_helper_spec.rb
+++ spec/helpers/hash_helper_spec.rb
@@ -1,28 +1,24 @@
-require './app/helpers/hash_helper'
+require "rails_helper"
 
-RSpec.configure do |c|
-  c.include HashHelper
-end
+RSpec.describe HashHelper do
 
-RSpec.describe "HashHelper" do
-
-  context "#get_value" do
+  describe "#get_value" do
 
     it "has defined in HashHelper" do
       options = {}
-      expect(get_value :a, options).to be_nil
+      expect(helper.get_value :a, options).to be_nil
     end
 
     context "without give a default value argument" do
 
       it "can get a value from a hash by a existed key" do
         options = { a: '1' }
-        expect(get_value :a, options).to eq('1')
+        expect(helper.get_value :a, options).to eq('1')
       end
 
       it "should get nil if key is not existed in a hash" do
         options = { a: '1' }
-        expect(get_value :b, options).to be_nil
+        expect(helper.get_value :b, options).to be_nil
       end
     end
 
@@ -30,38 +26,38 @@ RSpec.describe "HashHelper" do
 
       it "should get the default value if the value is nil" do
         options = { a: nil }
-        expect(get_value :b, options, '1').to eq('1')
+        expect(helper.get_value :b, options, '1').to eq('1')
       end
 
       it "should get the default value if key is not existed in a hash" do
         options = { a: '1' }
-        expect(get_value :b, options, '2').to eq('2')
+        expect(helper.get_value :b, options, '2').to eq('2')
       end
     end
   end
 
-  context "#pop_value" do
+  describe "#pop_value" do
 
     it "has defined in HashHelper" do
       options = {}
-      expect(pop_value :a, options).to be_nil
+      expect(helper.pop_value :a, options).to be_nil
     end
 
     context "without a default value argument" do
 
       it "can get a value from a hash by a existed key" do
         options = { 'a' => '1' }
-        expect(pop_value :a, options).to eq('1')
+        expect(helper.pop_value :a, options).to eq('1')
       end
 
       it "should get nil if key is not existed in a hash" do
         options = { a: '1' }
-        expect(pop_value :b, options).to be(nil)
+        expect(helper.pop_value :b, options).to be(nil)
       end
 
       it "the key should be disappear from the hash" do |variable|
         options = { a: '1' }
-        pop_value :a, options
+        helper.pop_value :a, options
         expect(options).to eq({})
       end
     end
@@ -70,26 +66,26 @@ RSpec.describe "HashHelper" do
 
       it "should get the default value if the value is nil" do
         options = { a: nil }
-        expect(pop_value :b, options, '1').to eq('1')
+        expect(helper.pop_value :b, options, '1').to eq('1')
       end
 
       it "should get the default value if key is not existed in a hash" do
         options = { a: '1' }
-        expect(pop_value :b, options, '2').to eq('2')
+        expect(helper.pop_value :b, options, '2').to eq('2')
       end
     end
   end
 
-  context "#symbolize_keys" do
+  describe "#symbolize_keys" do
 
     it "has defined in HashHelper" do
       options = {}
-      expect(symbolize_keys options).to eq({})
+      expect(helper.symbolize_keys options).to eq({})
     end
 
     it "should get a new hash that all keys are symbol " do
       options = { 'a' => '1', 'b' => '2', 'c' => '3' }
-      expect(symbolize_keys options).to eq({ a: '1', b: '2', c: '3' })
+      expect(helper.symbolize_keys options).to eq({ a: '1', b: '2', c: '3' })
     end
   end
 end
